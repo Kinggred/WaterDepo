cac583311b65715f9cfe16ee8ac4623e7c17a3eb
Maksymilian Grupiński <maks.grupinski@solve.md>
2023-12-01 22:43:23 +0100

    This doesnt work

--- src/main/java/Controllers/KayakModelController.java
+++ src/main/java/Controllers/KayakModelController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,8 +44,17 @@ public class KayakModelController {
 
     @ResponseBody
     @GetMapping
-    public ResponseEntity getAvailableKayakModels() {
+    public List<KayakModel> getAvailableKayakModels() {
         List<KayakModel> models = kayakModelService.getAllKayakModels();
-        return ResponseEntity.ok(models);
+        return models;    }
+
+    @ResponseBody
+    @GetMapping("/{id}")
+    public KayakModel getSingleKayakModel(@PathVariable UUID id) {
+        try {
+            return kayakModelService.getKayakModelById(id);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
