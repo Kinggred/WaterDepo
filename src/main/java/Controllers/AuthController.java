ca575e344a84843ad49f19ada215290b7e15d7a5
Maksymilian Grupiński <maks.grupinski@solve.md>
2023-12-01 13:05:15 +0100

    Added better respoonse upon creating a user

--- src/main/java/Controllers/AuthController.java
+++ src/main/java/Controllers/AuthController.java
@@ -6,6 +6,9 @@ import Models.Login.LoginRes;
 import Models.User.User;
 import Services.UserService;
 import WaterDepo.auth.JwtUtil;
+
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -25,47 +28,50 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/rest/auth")
 public class AuthController {
 
-    @Autowired
-    private JwtUtil jwtUtil;
+  @Autowired private JwtUtil jwtUtil;
 
-    private UserService userService;
-    private final AuthenticationManager authenticationManager;
+  private UserService userService;
+  private final AuthenticationManager authenticationManager;
 
-    public AuthController(AuthenticationManager authenticationManager, UserService userService) {
-        this.authenticationManager = authenticationManager;
-        this.userService = userService;
-    }
+  public AuthController(AuthenticationManager authenticationManager, UserService userService) {
+    this.authenticationManager = authenticationManager;
+    this.userService = userService;
+  }
 
   @PostMapping("/register")
   public ResponseEntity createUser(@RequestBody User user) {
     try {
-    User dbUser = userService.createUser(user);
-    return ResponseEntity.ok(dbUser);
+      User userInDb = userService.createUser(user);
+      HashMap<String, String> response = new HashMap<>();
+      response.put("info", "User created");
+      response.put("email", userInDb.getEmail());
+      return ResponseEntity.ok(response);
     } catch (DataIntegrityViolationException e) {
-        ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "You mongol");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+      ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "You mongol");
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
   }
 
-    @ResponseBody
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity login(@RequestBody LoginReq loginReq) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
-            String email = authentication.getName();
-            User user = userService.getUserByEmail(email);
-            String token = jwtUtil.createToken(user);
-            LoginRes loginRes = new LoginRes(email, token);
-            return ResponseEntity.ok(loginRes);
-        } catch (BadCredentialsException e) {
-            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        } catch (Exception e) {
-            // TODO: I'm pretty sure this is unsafe AF
-            // TODO: Remove e.getMessage() Insert something generic
-            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }
+  @ResponseBody
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public ResponseEntity login(@RequestBody LoginReq loginReq) {
+    try {
+      Authentication authentication =
+          authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
+      String email = authentication.getName();
+      User user = userService.getUserByEmail(email);
+      String token = jwtUtil.createToken(user);
+      LoginRes loginRes = new LoginRes(email, token);
+      return ResponseEntity.ok(loginRes);
+    } catch (BadCredentialsException e) {
+      ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    } catch (Exception e) {
+      // TODO: I'm pretty sure this is unsafe AF
+      // TODO: Remove e.getMessage() Insert something generic
+      ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
+  }
 }
