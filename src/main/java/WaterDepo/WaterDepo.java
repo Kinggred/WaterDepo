b77410f02a0d1b1111f14d85aef697b0cf5585c8
Maksymilian Grupiński <maks.grupinski@solve.md>
2023-12-04 09:49:08 +0100

    Removed bean listing

--- src/main/java/WaterDepo/WaterDepo.java
+++ src/main/java/WaterDepo/WaterDepo.java
@@ -14,16 +14,4 @@ public class WaterDepo {
   public static void main(String[] args) {
     SpringApplication.run(WaterDepo.class, args);
   }
-
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx){
-      return args -> {
-          System.out.println("Bean inspection!?:");
-          String[] beanNames = ctx.getBeanDefinitionNames();
-          Arrays.sort(beanNames);
-          for (String beanName : beanNames) {
-              System.out.println(beanName);
-          }
-      };
-  }
 }
