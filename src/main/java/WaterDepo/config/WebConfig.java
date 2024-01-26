940ce4c272f4cf78e2bc7ef42310ffa9c83d6fa9
Maksymilian Grupiński <maks.grupinski@solve.md>
2024-01-26 14:06:02 +0100

    Added logging interceptor

--- src/main/java/WaterDepo/config/WebConfig.java
+++ src/main/java/WaterDepo/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
